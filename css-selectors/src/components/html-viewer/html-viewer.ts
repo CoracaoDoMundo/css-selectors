f551a9ee6d2a041073d7afa6e9da7543db7a6ec4
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-06-29 02:00:39 +0300

    feat: add highlight.js to the project

--- css-selectors/src/components/html-viewer/html-viewer.ts
+++ css-selectors/src/components/html-viewer/html-viewer.ts
@@ -1,13 +1,24 @@
 import { createElement } from '../service-functions';
+const hljs = require('highlight.js/lib/core');
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 
 class Viewer {
-    public viewer: HTMLDivElement = document.createElement('div');
+  public viewer: HTMLDivElement = document.createElement('div');
+  public preBlock: HTMLPreElement = document.createElement('pre');
 
-    draw(container:HTMLDivElement) {
-        this.viewer.classList.add('codeViewerBlock');
-        container.append(this.viewer);
-    }
+  draw(container: HTMLDivElement) {
+    this.viewer.classList.add('codeViewerBlock');
+    container.append(this.viewer);
+    this.preBlock.classList.add('codeTextContainer');
+    this.viewer.append(this.preBlock);
+    this.fillViewerField();
+  }
 
+  fillViewerField() {
+    const code:HTMLSpanElement = createElement('code', ['codeViewerText'], this.preBlock);
+    code.textContent = '<div class="blanket"><slates /><slates /></div>';
+    hljs.highlightBlock(this.preBlock);
+  }
 }
 
 export default Viewer;
